c77e171732581d774342d7167db735cb2f54057e
Yewon <ymh20804@naver.com>
2026-04-22 20:29:53 +0900

    GRT-63 [SETTING] storybook 세팅

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,22 +1,19 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from "eslint-plugin-storybook";
+
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
 import prettierPlugin from "eslint-plugin-prettier";
 import prettierConfig from "eslint-config-prettier";
 
-const eslintConfig = defineConfig([
-  ...nextVitals,
-  nextTs,
-  {
-    plugins: {
-      prettier: prettierPlugin,
-    },
-    rules: {
-      "prettier/prettier": "error",
-    },
+const eslintConfig = defineConfig([...nextVitals, nextTs, {
+  plugins: {
+    prettier: prettierPlugin,
   },
-  prettierConfig,
-  globalIgnores([".next/**", "out/**", "build/**", "next-env.d.ts"]),
-]);
+  rules: {
+    "prettier/prettier": "error",
+  },
+}, prettierConfig, globalIgnores([".next/**", "out/**", "build/**", "next-env.d.ts"]), ...storybook.configs["flat/recommended"]]);
 
 export default eslintConfig;
