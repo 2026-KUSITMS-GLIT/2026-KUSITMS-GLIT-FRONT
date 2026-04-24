b86d5e8bdf1e743f6a4d7e4ba9e65646c7ca6f10
YuminPark <p353700@gmail.com>
2026-04-24 15:46:52 +0900

    GRT-68 [FEAT] 아이콘 추가 및 dimension: false 속성 세팅

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,16 @@
+import EyeOpenIcon from "@/assets/icons/icon_eye_open.svg";
+import EyeCloseIcon from "@/assets/icons/icon_eye_closed.svg";
+import ChevronLeftIcon from "@/assets/icons/icon_chevron_left.svg";
+
 const page = () => {
-  return <p className="head-1 text-sea-blue-600">Glit</p>;
+  return (
+    <div>
+      <EyeOpenIcon className="text-sea-blue-600 size-8" />
+      <EyeCloseIcon className="text-tag-100 size-5" />
+      <ChevronLeftIcon className="text-tag-300 size-6" />
+      <p className="head-1 text-sea-blue-600">글릿</p>
+    </div>
+  );
 };
 
 export default page;
