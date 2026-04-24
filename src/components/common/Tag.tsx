49af43b3d2190a11fef25ceb9638d79f2b97ea8d
YuminPark <p353700@gmail.com>
2026-04-24 19:00:15 +0900

    GRT-68 [FEAT] 누락된 Semantic 토큰 추가

--- src/components/common/Tag.tsx
+++ src/components/common/Tag.tsx
@@ -6,7 +6,7 @@ const VARIANT_STYLES: Record<TagVariant, string> = {
   gray: "bg-gray-800 text-offwhite-400",
   tag100: "bg-tag-100 text-offwhite-400",
   tag200: "bg-tag-200 text-offwhite-400",
-  tag300: "bg-tag-300 text-black",
+  tag300: "bg-tag-300 text-typo-primary",
   tag400: "bg-tag-400 text-white",
   tag500: "bg-tag-500 text-white",
 };
