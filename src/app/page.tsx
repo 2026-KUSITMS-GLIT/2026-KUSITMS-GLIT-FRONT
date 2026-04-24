db0d15fc140f4433a54dce7ce933693d625808c7
YuminPark <p353700@gmail.com>
2026-04-24 18:30:25 +0900

    GRT-68 [CHORE] 루트 페이지 원상복구

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,5 @@
-import EyeOpenIcon from "@/assets/icons/icon_eye_open.svg";
-import EyeCloseIcon from "@/assets/icons/icon_eye_closed.svg";
-import ChevronLeftIcon from "@/assets/icons/icon_chevron_left.svg";
-import ProgressBar from "@/components/common/ProgressBar";
-import Tag from "@/components/common/Tag";
-
 const page = () => {
-  return (
-    <div>
-      <EyeOpenIcon className="text-sea-blue-600 size-8" />
-      <EyeCloseIcon className="text-tag-100 size-5" />
-      <ChevronLeftIcon className="text-tag-300 size-6" />
-      <p className="head-1 text-sea-blue-600">글릿</p>
-      <div className="flex w-100 flex-col gap-4">
-        <ProgressBar step={1} />
-        <ProgressBar step={2} />
-        <ProgressBar step={3} />
-      </div>
-      <div className="flex w-full flex-col gap-2">
-        <Tag variant="tag300">로그인하기</Tag>
-        <Tag variant="tag400">
-          <EyeCloseIcon />
-          tag100
-        </Tag>
-      </div>
-    </div>
-  );
+  return <p className="head-1 text-sea-blue-600">Glit</p>;
 };
 
 export default page;
