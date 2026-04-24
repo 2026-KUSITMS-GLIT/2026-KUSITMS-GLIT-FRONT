3dfddbeefe7b635bf5f34239b33cb44791d49c1c
YuminPark <p353700@gmail.com>
2026-04-24 18:09:19 +0900

    GRT-68 [FEAT] Tag 컴포넌트 구현

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import EyeOpenIcon from "@/assets/icons/icon_eye_open.svg";
 import EyeCloseIcon from "@/assets/icons/icon_eye_closed.svg";
 import ChevronLeftIcon from "@/assets/icons/icon_chevron_left.svg";
 import ProgressBar from "@/components/common/ProgressBar";
+import Tag from "@/components/Tag";
 
 const page = () => {
   return (
@@ -14,6 +15,13 @@ const page = () => {
         <ProgressBar step={1} />
         <ProgressBar step={2} />
         <ProgressBar step={3} />
+      </div>
+      <div className="flex w-full flex-col gap-2">
+        <Tag variant="tag300">로그인하기</Tag>
+        <Tag variant="tag400">
+          <EyeCloseIcon />
+          태그명
+        </Tag>
       </div>
     </div>
   );
