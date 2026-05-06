8b31a3a29135482dbd1c30aecf3f0156112f892b
Yewon <ymh20804@naver.com>
2026-05-06 16:34:11 +0900

    GRT-97 [MOD] 온보딩 이미지 확장자 PNG로 변경

--- src/data/onboarding.tsx
+++ src/data/onboarding.tsx
@@ -1,34 +1,38 @@
-import Applicant from "@/assets/images/onboarding/applicant.svg";
-import ApplicantSelected from "@/assets/images/onboarding/applicant-selected.svg";
-import Designer from "@/assets/images/onboarding/designer.svg";
-import DesignerSelected from "@/assets/images/onboarding/designer-selected.svg";
-import Developer from "@/assets/images/onboarding/developer.svg";
-import DeveloperSelected from "@/assets/images/onboarding/developer-selected.svg";
-import Employee from "@/assets/images/onboarding/employee.svg";
-import EmployeeSelected from "@/assets/images/onboarding/employee-selected.svg";
-import Planner from "@/assets/images/onboarding/planner.svg";
-import PlannerSelected from "@/assets/images/onboarding/planner-selected.svg";
-import Student from "@/assets/images/onboarding/student.svg";
-import StudentSelected from "@/assets/images/onboarding/student-selected.svg";
+import Image from "next/image";
+
+import ApplicantImg from "@/assets/images/onboarding/applicant.png";
+import ApplicantSelectedImg from "@/assets/images/onboarding/applicant-selected.png";
+import DesignerImg from "@/assets/images/onboarding/designer.png";
+import DesignerSelectedImg from "@/assets/images/onboarding/designer-selected.png";
+import DeveloperImg from "@/assets/images/onboarding/developer.png";
+import DeveloperSelectedImg from "@/assets/images/onboarding/developer-selected.png";
+import EmployeeImg from "@/assets/images/onboarding/employee.png";
+import EmployeeSelectedImg from "@/assets/images/onboarding/employee-selected.png";
+import PlannerImg from "@/assets/images/onboarding/planner.png";
+import PlannerSelectedImg from "@/assets/images/onboarding/planner-selected.png";
+import StudentImg from "@/assets/images/onboarding/student.png";
+import StudentSelectedImg from "@/assets/images/onboarding/student-selected.png";
 
 export const JOB_OPTIONS = [
   {
     value: "디자이너",
     label: "디자이너",
-    icon: <Designer />,
-    selectedIcon: <DesignerSelected />,
+    icon: <Image src={DesignerImg} alt="디자이너" fill className="object-contain" />,
+    selectedIcon: (
+      <Image src={DesignerSelectedImg} alt="디자이너" fill className="object-contain" />
+    ),
   },
   {
     value: "기획자",
     label: "기획자",
-    icon: <Planner />,
-    selectedIcon: <PlannerSelected />,
+    icon: <Image src={PlannerImg} alt="기획자" fill className="object-contain" />,
+    selectedIcon: <Image src={PlannerSelectedImg} alt="기획자" fill className="object-contain" />,
   },
   {
     value: "개발자",
     label: "개발자",
-    icon: <Developer />,
-    selectedIcon: <DeveloperSelected />,
+    icon: <Image src={DeveloperImg} alt="개발자" fill className="object-contain" />,
+    selectedIcon: <Image src={DeveloperSelectedImg} alt="개발자" fill className="object-contain" />,
   },
 ];
 
@@ -36,19 +40,21 @@ export const STATUS_OPTIONS = [
   {
     value: "재학 중",
     label: "재학중",
-    icon: <Student />,
-    selectedIcon: <StudentSelected />,
+    icon: <Image src={StudentImg} alt="재학중" fill className="object-contain" />,
+    selectedIcon: <Image src={StudentSelectedImg} alt="재학중" fill className="object-contain" />,
   },
   {
     value: "취업 준비중",
     label: "취업 준비중",
-    icon: <Applicant />,
-    selectedIcon: <ApplicantSelected />,
+    icon: <Image src={ApplicantImg} alt="취업 준비중" fill className="object-contain" />,
+    selectedIcon: (
+      <Image src={ApplicantSelectedImg} alt="취업 준비중" fill className="object-contain" />
+    ),
   },
   {
     value: "재직 중",
     label: "재직중",
-    icon: <Employee />,
-    selectedIcon: <EmployeeSelected />,
+    icon: <Image src={EmployeeImg} alt="재직중" fill className="object-contain" />,
+    selectedIcon: <Image src={EmployeeSelectedImg} alt="재직중" fill className="object-contain" />,
   },
 ];
