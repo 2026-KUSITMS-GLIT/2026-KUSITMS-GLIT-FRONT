591f82a91239456a39735b0e14ebf6d282fd0270
Yewon <ymh20804@naver.com>
2026-05-07 12:54:38 +0900

    GRT-97 [MOD] 온보딩 카드 컴포넌트 cursor-pointer 명시

--- src/components/onboarding/SelectionCard.tsx
+++ src/components/onboarding/SelectionCard.tsx
@@ -22,7 +22,7 @@ const SelectionCard = ({
       type="button"
       onClick={onClick}
       className={cn(
-        "rounded-16 bg-gray-850 relative flex h-40 w-40 flex-col items-center justify-start gap-2 pt-3 pb-5 transition-all",
+        "rounded-16 bg-gray-850 relative flex h-40 w-40 cursor-pointer flex-col items-center justify-start gap-2 pt-3 pb-5 transition-all",
         selected
           ? "border-sea-blue-500 border-[1.6px] opacity-100"
           : "border-[1.6px] border-transparent opacity-[0.76]",
