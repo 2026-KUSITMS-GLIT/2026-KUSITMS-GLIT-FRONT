d29f63d81024775682b038434af296efa82ec4fd
YuminPark <p353700@gmail.com>
2026-05-09 21:04:59 +0900

    GRT-104 [FEAT] 히트맵 인디케이터 구현 및 연결

--- src/components/home/HeatmapSection.tsx
+++ src/components/home/HeatmapSection.tsx
@@ -1,13 +1,19 @@
 import Heatmap from "@/components/home/Heatmap";
-import { mockHeatmapData } from "@/data/heatmap";
+import HeatmapIndicator from "@/components/home/HeatmapIndicator";
+import { mockHeatmapData, mockHeatmapDataList } from "@/data/heatmap";
 
 const HeatmapSection = () => {
   const month = Number(mockHeatmapData.month.split("-")[1]);
+  const total = mockHeatmapDataList.length;
+  const current = mockHeatmapDataList.findIndex(d => d.month === mockHeatmapData.month);
 
   return (
-    <div className="bg-card rounded-12 flex w-full flex-col gap-4 p-4">
-      <p className="body-3 text-white">{month}월 기록 출석부</p>
-      <Heatmap data={mockHeatmapData} />
+    <div className="flex flex-col gap-2">
+      <div className="bg-card rounded-12 flex w-full flex-col gap-4 p-4">
+        <p className="body-3 text-white">{month}월 기록 출석부</p>
+        <Heatmap data={mockHeatmapData} />
+      </div>
+      <HeatmapIndicator total={total} current={current} />
     </div>
   );
 };
