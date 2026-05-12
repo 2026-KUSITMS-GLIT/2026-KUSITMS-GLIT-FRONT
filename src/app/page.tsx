00ede280a11f6bd19e9f60d684d6356f21af9158
YuminPark <p353700@gmail.com>
2026-05-13 00:16:07 +0900

    GRT-104 [FEAT] 홈 임시 캐릭터 이미지 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 
+import CharacterHome from "@/assets/images/home/character_home.svg";
 import CTA from "@/components/common/CTA";
 import NavigationBar from "@/components/common/NavigationBar";
 import HeatmapSection from "@/components/home/HeatmapSection";
@@ -8,11 +9,12 @@ import RadarChartSection from "@/components/home/RadarChartSection";
 const page = () => {
   return (
     <div className="flex h-full flex-col">
-      <div className="scrollbar-hide flex-1 overflow-y-auto px-5 pt-12">
-        <p className="head-5 pb-3 text-center text-white">
+      <div className="scrollbar-hide flex-1 overflow-y-auto px-5 pt-12 pb-6">
+        <p className="head-5 pb-3.5 text-center text-white">
           오늘의 경험을 기록하고 <br /> 다솔님의 강점을 확인해보세요
         </p>
-        {/* gif */}
+        {/* TODO: gif로 추후 수정 */}
+        <CharacterHome className="mx-auto block h-45.5 w-57" />
         <div className="flex flex-col gap-7 pt-2">
           <Link href="/record/today-task">
             <CTA>기록하러 가기</CTA>
