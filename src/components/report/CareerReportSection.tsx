844eaec430d3611db1c78c8208b276afaab81e1b
YuminPark <p353700@gmail.com>
2026-05-14 19:39:09 +0900

    GRT-115 [CHORE] 지금 기록하러 가기 버튼 경로 연결

--- src/components/report/CareerReportSection.tsx
+++ src/components/report/CareerReportSection.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 import { StarTwoIcon } from "@/assets/icons";
 import Button from "@/components/common/Button";
 import ReportCard from "@/components/report/ReportCard";
@@ -21,7 +23,9 @@ const CareerReportSection = ({ reports = mockReports }: CareerReportSectionProps
                 <StarTwoIcon className="size-15 text-gray-700" />
                 <p className="body-2 text-gray-800">아직 발행된 리포트가 없어요!</p>
               </div>
-              <Button className="bg-white active:bg-gray-300">지금 기록하러 가기</Button>
+              <Link href="/record">
+                <Button className="bg-white active:bg-gray-300">지금 기록하러 가기</Button>
+              </Link>
             </div>
           </div>
         )}
