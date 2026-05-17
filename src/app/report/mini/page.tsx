757f7e800fc6eb39f8abc6e0ed94dcbb15c56992
YuminPark <p353700@gmail.com>
2026-05-17 19:46:18 +0900

    GRT-121 [FEAT] MoreStep 컴포넌트 구현

--- src/app/report/mini/page.tsx
+++ src/app/report/mini/page.tsx
@@ -4,6 +4,7 @@ import { useRouter } from "next/navigation";
 
 import Header from "@/components/common/Header";
 import CompetencyStatsSection from "@/components/report/CompetencyStatsSection";
+import MoreStep from "@/components/report/MoreStep";
 import TopDetailTagsSection from "@/components/report/TopDetailTagsSection";
 import { mockReportDetail } from "@/data/report";
 
@@ -14,21 +15,24 @@ const Page = () => {
     <div className="w-full">
       <Header title="미니 리포트" onLeftClick={() => router.push("/report")} />
       <div className="px-5 pt-4">
-        <div className="flex flex-col gap-4">
-          <div className="flex flex-col gap-3">
-            <div>
-              <p className="body-5 pb-0.5 text-gray-600">2026.04.16</p>
-              <p className="head-4 pb-2 text-gray-100">다솔님의 미니 리포트가 나왔어요</p>
-              <p className="body-5 text-sea-blue-500">벌써 10개의 심화기록이 쌓였어요!</p>
-              <p className="body-5 text-gray-300">얼마나 열심히 기록했는지 확인해볼까요?</p>
+        <div className="flex flex-col gap-1.5">
+          <div className="flex flex-col gap-4">
+            <div className="flex flex-col gap-3">
+              <div>
+                <p className="body-5 pb-0.5 text-gray-600">2026.04.16</p>
+                <p className="head-4 pb-2 text-gray-100">다솔님의 미니 리포트가 나왔어요</p>
+                <p className="body-5 text-sea-blue-500">벌써 10개의 심화기록이 쌓였어요!</p>
+                <p className="body-5 text-gray-300">얼마나 열심히 기록했는지 확인해볼까요?</p>
+              </div>
+              <CompetencyStatsSection
+                topCategories={mockReportDetail.content.competencyStats.topCategories}
+              />
             </div>
-            <CompetencyStatsSection
-              topCategories={mockReportDetail.content.competencyStats.topCategories}
+            <TopDetailTagsSection
+              topDetailTags={mockReportDetail.content.competencyStats.topDetailTags}
             />
           </div>
-          <TopDetailTagsSection
-            topDetailTags={mockReportDetail.content.competencyStats.topDetailTags}
-          />
+          <MoreStep />
         </div>
       </div>
     </div>
---
+++ src/components/report/MoreStep.tsx
@@ -0,0 +1,12 @@
+const MoreStep = () => {
+  return (
+    <div className="flex flex-col items-center gap-1.5 self-center">
+      <div className="bg-gray-850 size-1.5 rounded-full opacity-60"></div>
+      <div className="size-1.5 rounded-full bg-gray-800 opacity-80"></div>
+      <div className="size-1.5 rounded-full bg-gray-700 opacity-80"></div>
+      <div className="size-1.5 rounded-full bg-gray-400 opacity-80"></div>
+    </div>
+  );
+};
+
+export default MoreStep;
