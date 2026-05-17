aa8e5a3d6b2ebd9a739c5de33175d076ff292d43
YuminPark <p353700@gmail.com>
2026-05-17 20:47:41 +0900

    GRT-121 [REFACTOR] 헤더 아래 미니 리포트 부분만 세로스크롤

--- src/app/report/mini/page.tsx
+++ src/app/report/mini/page.tsx
@@ -15,9 +15,9 @@ const Page = () => {
   const router = useRouter();
 
   return (
-    <div className="w-full">
+    <div className="flex h-screen w-full flex-col">
       <Header title="미니 리포트" onLeftClick={() => router.push("/report")} />
-      <div className="px-5 pt-4">
+      <div className="scrollbar-hide flex-1 overflow-y-auto px-5 pt-4 pb-4">
         <div className="flex flex-col gap-6">
           <div className="flex flex-col gap-1.5">
             <div className="flex flex-col gap-4">
