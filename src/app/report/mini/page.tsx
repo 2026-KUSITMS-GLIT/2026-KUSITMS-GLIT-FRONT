774d02524391f353321fe6c15145ab2cb12aba5f
YuminPark <p353700@gmail.com>
2026-05-17 17:37:27 +0900

    [FEAT] 미니 리포트 역량 기록 현황 섹션잡기

---
+++ src/app/report/mini/page.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
+import Header from "@/components/common/Header";
+import CompetencyStatsSection from "@/components/report/CompetencyStatsSection";
+
+const Page = () => {
+  const router = useRouter();
+
+  return (
+    <div className="w-full">
+      <Header title="미니 리포트" onLeftClick={() => router.push("/report")} />
+      <div className="px-5 pt-4">
+        <div className="flex flex-col gap-3">
+          <div>
+            <p className="body-4 pb-0.5 text-gray-600">2026.04.16</p>
+            <p className="head-4 pb-2 text-gray-100">다솔님의 미니 리포트가 나왔어요</p>
+            <p className="body-4 text-sea-blue-500">벌써 10개의 심화기록이 쌓였어요!</p>
+            <p className="body-4 text-gray-300">얼마나 열심히 기록했는지 확인해볼까요?</p>
+          </div>
+          <CompetencyStatsSection />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Page;
---
+++ src/components/report/CompetencyStatsSection.tsx
@@ -0,0 +1,5 @@
+const CompetencyStatsSection = () => {
+  return <div className="p-4 text-white">CompetencyStatsSection</div>;
+};
+
+export default CompetencyStatsSection;
