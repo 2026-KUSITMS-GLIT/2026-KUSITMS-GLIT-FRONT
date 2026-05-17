8ead48320b50dee785894a7eb46aef03cfb37041
YuminPark <p353700@gmail.com>
2026-05-17 20:36:59 +0900

    GRT-121 [REFACTOR] 지금까지의 활동요약 mockData 연결

--- src/components/report/ActivitySummarySection.tsx
+++ src/components/report/ActivitySummarySection.tsx
@@ -1,5 +1,16 @@
+import { mockReportDetail } from "@/data/report";
+
 const ActivitySummarySection = () => {
-  return <div className="rounded-12 bg-gray-850 p-4 text-white">ActivitySummarySection</div>;
+  const { activitySummary } = mockReportDetail.content;
+
+  return (
+    <div className="rounded-12 bg-gray-850 p-4 text-white">
+      <div className="flex flex-col gap-2">
+        <p className="body-3 text-gray-100">지금까지의 활동요약</p>
+        <p className="body-4 text-gray-400">&ldquo;{activitySummary}&rdquo;</p>
+      </div>
+    </div>
+  );
 };
 
 export default ActivitySummarySection;
