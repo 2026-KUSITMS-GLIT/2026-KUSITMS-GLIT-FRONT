59a1fa23e6e423c2eba8d1647194410299f78b2e
YuminPark <p353700@gmail.com>
2026-05-17 18:33:16 +0900

    GRT-121 [FIX] 라벨 줄바꿈 되지 않도록 수정

--- src/components/report/CompetencyStatsSection.tsx
+++ src/components/report/CompetencyStatsSection.tsx
@@ -28,7 +28,7 @@ const CompetencyStatsSection = ({ topCategories = [] }: Props) => {
         return (
           <div key={category} className="flex flex-1 flex-col items-center">
             <Icon className="size-9" />
-            <p className="body-5 pt-0.75 text-gray-400">{label}</p>
+            <p className="body-5 pt-0.75 whitespace-nowrap text-gray-400">{label}</p>
             <p className="body-3 text-gray-100">{count}회</p>
           </div>
         );
