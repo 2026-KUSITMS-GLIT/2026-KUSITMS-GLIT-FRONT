da429251916124961f9ec7e382268fb52a1933de
YuminPark <p353700@gmail.com>
2026-05-23 13:06:27 +0900

    [FEAT] 역량 잔디 조회 API 연동

--- src/lib/apis/home/home.ts
+++ src/lib/apis/home/home.ts
@@ -1,8 +1,9 @@
 import { api } from "@/api/client";
-import type { ActivityStatsResponse, MonthlyGrassResponse } from "@/types/home/home";
+import type { ActivityStatsResponse, MonthlyGrassData } from "@/types/home/home";
 
 // 역량 레이더 차트 조회
 export const getRadar = () => api.get<ActivityStatsResponse>("/api/home/radar");
 
 // 월별 역량 잔디 조회
-export const getCompetencyStats = () => api.get<MonthlyGrassResponse>("/api/home/competency-stats");
+export const getCompetencyStats = (month: string) =>
+  api.get<MonthlyGrassData>("/api/home/competency-stats", { month });
