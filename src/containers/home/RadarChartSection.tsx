0ca9f7f7bd1cb38d9b4999fb6cc4bb8e4be08094
YuminPark <p353700@gmail.com>
2026-05-24 21:21:22 +0900

    [FIX] Hydration 에러 해결

--- src/containers/home/RadarChartSection.tsx
+++ src/containers/home/RadarChartSection.tsx
@@ -31,7 +31,9 @@ const RadarChartSection = () => {
 
   return (
     <div className="bg-card rounded-12 flex w-full flex-col p-4">
-      <p className="body-3 text-white">{me?.nickname ?? ""}님의 역량 기록 분포</p>
+      <p className="body-3 text-white">
+        <span suppressHydrationWarning>{me?.nickname ?? ""}</span>님의 역량 기록 분포
+      </p>
       <div className="flex items-center justify-center">
         <RadarChart data={data ?? emptyData} />
       </div>
