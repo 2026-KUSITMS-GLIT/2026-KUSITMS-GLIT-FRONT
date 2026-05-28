018e811a597bc114e4eaaecc300e4afefd4b4627
YuminPark <p353700@gmail.com>
2026-05-28 17:28:03 +0900

    [CHORE] 텍스트 필드에 ml, mt 추가

--- src/containers/my/profile/ProfileForm.tsx
+++ src/containers/my/profile/ProfileForm.tsx
@@ -83,7 +83,7 @@ const ProfileForm = ({ initialProfile }: ProfileFormProps) => {
               disabled={!isEditing}
               maxLength={12}
               className="h-9.75 p-2"
-              textareaClassName="h-5.75"
+              textareaClassName="h-5.75 mt-0.5 ml-1"
               showCount={false}
               onChange={isEditing ? val => setNickname(val) : undefined}
               onBlur={isEditing ? () => setNicknameTouched(true) : undefined}
