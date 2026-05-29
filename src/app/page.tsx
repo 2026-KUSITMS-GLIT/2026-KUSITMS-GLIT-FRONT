ba8f2eee8e4ffa04980b1de53600c90c4c31ca7f
YuminPark <p353700@gmail.com>
2026-05-29 17:08:06 +0900

    [FIX] 텍스트를 블러 위에 흐려지지 않게 렌더링

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -63,7 +63,7 @@ const Page = () => {
         />
       ) : null}
       <div className="scrollbar-hide flex-1 overflow-y-auto px-5 pt-12 pb-6">
-        <p className="head-5 pb-3.5 text-center text-white">
+        <p className="head-5 relative z-10 pb-3.5 text-center text-white">
           오늘의 경험을 기록하고 <br />
           <span suppressHydrationWarning>{me?.nickname ?? ""}</span>
           님의 강점을 확인해보세요
