94b305c6d1fc664ff6687222b163eaeb10cfb514
Yewon <ymh20804@naver.com>
2026-06-02 07:08:45 +0900

    [MOD] user API 함수명 컨벤션 적용

--- src/lib/apis/user/notification.ts
+++ src/lib/apis/user/notification.ts
@@ -2,8 +2,9 @@ import { api } from "@/api/client";
 import type { AlarmData } from "@/types/user/notification";
 
 // 내 알림 설정 조회
-export const getAlarmSettings = () => api.get<AlarmData>("/api/users/me/notification-settings");
+export const getNotificationSettings = () =>
+  api.get<AlarmData>("/api/users/me/notification-settings");
 
 // 내 알림 설정 저장
-export const patchAlarmSettings = (body: Partial<AlarmData>) =>
+export const patchNotificationSettings = (body: Partial<AlarmData>) =>
   api.patch("/api/users/me/notification-settings", body);
