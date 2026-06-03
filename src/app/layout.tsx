aef7864572dd83f0450d3a8057c465bbb98d3419
Yewon <ymh20804@naver.com>
2026-06-03 16:38:14 +0900

    [MOD] API preconnect에 crossOrigin 속성 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -59,7 +59,9 @@ export default function RootLayout({
     <html
       lang="ko"
       className={`h-dvh overflow-hidden bg-gray-900 font-sans ${pretendard.variable}`}>
-      <head>{apiOrigin && <link rel="preconnect" href={apiOrigin} crossOrigin="anonymous" />}</head>
+      <head>
+        {apiOrigin && <link rel="preconnect" href={apiOrigin} crossOrigin="use-credentials" />}
+      </head>
       <body className="app-viewport-bg h-dvh overflow-hidden">
         <Providers>
           <main className="relative z-10 mx-auto flex h-dvh w-full max-w-107.5 min-w-0 overflow-hidden bg-gray-900">
